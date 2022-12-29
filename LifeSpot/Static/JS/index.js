7340e97d001a30e6ed180e4082df88c05e51c301
Baltram <Baltram@DESKTOP-4UU4P38>
2022-12-30 02:51:53 +0600

    V1.4

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -13,35 +13,9 @@ let sessionLog = function logSession() {
 
 let session = new Map();
 
-function getReview() {
-    // Создадим объект
-    let review = {}
 
-    // Сохраним свойство имени
-    review["userName"] = prompt("Как вас зовут ?")
-    if (review["userName"] == null) {
-        return
-    }
 
-    // Сохраним текст отзыва
-    review["comment"] = prompt("Напишите свой отзыв")
-    if (review["comment"] == null) {
-        return
-    }
 
-    // Сохраним текущее время
-    review["date"] = new Date().toLocaleString()
-
-    // Добавим на страницу
-    writeReview(review)
-}
-
-const writeReview = review => {
-    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
-        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
-        `<p>${review['comment']}</p>` +
-        '</div>';
-}
 
 setTimeout(() =>
     alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
