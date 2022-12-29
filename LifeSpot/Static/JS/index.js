bc780a300ec8048eb0d3056681a6563eddc6207a
Baltram <Baltram@DESKTOP-4UU4P38>
2022-12-30 02:36:23 +0600

    V1.3

--- LifeSpot/Static/JS/index.js
+++ LifeSpot/Static/JS/index.js
@@ -1,53 +1,72 @@
-﻿function handleSession() 
-{
-// создадим объект Map для хранения сессии
+﻿function handleSession() {
+    // Сохраним время начала сессии
+    session.set("startDate", new Date().toLocaleString())
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+}
+
+let sessionLog = function logSession() {
+    for (let result of session) {
+        console.log(result)
+    }
+}
+
 let session = new Map();
-// Сохраним UserAgent
-session.set("userAgent", window.navigator.userAgent)
 
+function getReview() {
+    // Создадим объект
+    let review = {}
 
-// Запросим возраст пользователя и тоже сохраним
-session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
 
-// Проверка на возраст и сохранение сессии
-if (session.get("age") >= 18) {
-    let startDate = new Date().toLocaleString();
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
 
-    alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
-    session.set("startDate", startDate)
-}
-else {
-    alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
-    window.location.href = "http://www.google.com"
-    a = true + 20 + "name"
-}
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
 
-// Вывод в консоль
-for (let result of session) {
-    console.log(result)
-}
+    // Добавим на страницу
+    writeReview(review)
 }
 
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
 
+setTimeout(() =>
+    alert("Нравится LifeSpot? " + '\n' + "Подпишитесь на наш Instagram @lifespot999!"),
+    30000);
 
 function filterContent() {
-
-    // Сохраняем текст пользовательского запроса.
-    let inputString = document.getElementsByTagName('input')[0].value.toLowerCase();
-    // Находим контейнеры с видео, которые необходимо фильтровать
     let elements = document.getElementsByClassName('video-container');
 
-    // Пробегаемся по контейнерам
     for (let i = 0; i <= elements.length; i++) {
-        // Вытаскиваем текст описания видео, которое необходимо отфильтровать
         let videoText = elements[i].querySelector(".video-title").innerText;
-        // Выполняем фильтрацию, сравнивая значения в нижнем регистре
-        if (!videoText.toLowerCase().includes(inputString.toLowerCase())) {
-            // Скрываем неподходящие
+        if (!videoText.toLowerCase().includes(inputParseFunction() /* Захват переменной теперь происходит с помощью замыкания */.toLowerCase())) {
             elements[i].style.display = 'none';
         } else {
-            // Показываем подходящие
             elements[i].style.display = 'inline-block';
         }
+    }
+}
+function checkAge() {
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    if (session.get("age") >= 18) {
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + new Date().toLocaleString());
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
     }
 }
